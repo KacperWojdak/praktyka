b596e66d68e0236f3f6bdc2667df315c9110b229
KacperWojdak <kacper.wojdak@interia.pl>
2022-06-08 08:48:59 +0200

    Login / Register page

---
+++ angular_baza/angular-baza/src/app/guard.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+
+  constructor(private dataservice: ApiService, private router: Router){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): any {
+    //return true;
+  const routeurl: string = state.url;
+  return this.isLogin(routeurl);
+
+  }
+
+  isLogin(routeurl: string){
+    if (this.dataservice.isLoggedIn()){
+      return true;
+    }
+
+    this.dataservice.redirectUrl = routeurl;
+    this.router.navigate(['/login'], {queryParams: {returnUrl: routeurl}} );
+
+  }
+  
+}
